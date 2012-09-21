acddefc6bba67560b39bba63581fa425d698f772
Travis Lewis <econobeing5@gmail.com>
2012-09-20 18:08:19 -0700

    attempting to create a variant

---
+++ src/TreadmillScreen.h
@@ -0,0 +1,12 @@
+/*
+ * TreadmillScreen.h
+ *
+ * Author: Travis Lewis
+ * Created on: 20 Sep 2012
+ */
+
+#ifndef TREADMILLSCREEN_H_
+#define TREADMILLSCREEN_H_
+
+
+#endif /* TREADMILLSCREEN_H_ */
